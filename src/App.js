6afb179859153db8d396d49718ae300cf7391042
fajesha nasim <fajesha1@gmail.com>
2023-09-06 18:33:25 -0500

    cleaned up the console logs

--- src/App.js
+++ src/App.js
@@ -1,91 +1,72 @@
 // break into components and see why we cant remove the event listner
-import './App.css';
-import { useState } from 'react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ModalComponent from './ModalComponent';
+import "./App.css";
+import { useState } from "react";
+import ModalComponent from "./ModalComponent";
 
 function App() {
- 
-  const [name, setName] = useState("");//name input
-  const [color, setColor] = useState("");//color input
-  const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
+	const [name, setName] = useState(""); //name input
+	const [color, setColor] = useState(""); //color input
+	const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
 
-  const nameChangeHandler=(e)=>{
-    console.log("nameChange" +e.target.value)
-    setName(e.target.value);
+	const nameChangeHandler = (e) => {
+		console.log("nameChange" + e.target.value);
+		setName(e.target.value);
+	};
 
-  }
-  
-  const colorChangeHandler=(e)=>{
-    console.log("colorChange");
-   console.log("colorChange" + e.target.value)
-    setColor(e.target.value);
-   
-  }
-  const openModal = () => {
-    setIsModalOpen(true);
-  }
+	const colorChangeHandler = (e) => {
+		console.log("colorChange" + e.target.value);
+		setColor(e.target.value);
+	};
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    window.removeEventListener('keydown', handleEscapeKey);
-    setIsModalOpen(false);
-    // setName("");
-    // setColor("");
+	const closeModal = () => {
+		window.removeEventListener("keydown", handleEscapeKey);
+		setIsModalOpen(false);
+		// setName("");
+		// setColor("");
+	};
 
-  }
-   
-  // Function to close modal on Esc key press
-  const handleEscapeKey=(e)=>{
-    if (e.key === 'Escape') {
-          closeModal();
-    }//else {alert("you pressed a key "+ e.key)}
+	// Function to close modal on Esc key press
+	const handleEscapeKey = (e) => {
+		if (e.key === "Escape") {
+			closeModal();
+		} //else {alert("you pressed a key "+ e.key)}
+	};
 
-  }
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
+		openModal(); // Open the modal when the form is submitted
+		window.addEventListener("keydown", handleEscapeKey);
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				{isModalOpen && (
+					<ModalComponent onConfirm={closeModal} name={name} color={color} />
+				)}
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    openModal(); // Open the modal when the form is submitted
-    window.addEventListener('keydown', handleEscapeKey);
-  }
-  ;
-  // div> 
-  //              <div className="backdrop" onClick={closeModal} > </div>
-  //              <div className="modal" >
-  //                 <h2>Hello, {name}!</h2>
-  //                 <p>Your favorite color is {color}.</p>
-  //                 <button onClick={closeModal}>Close</button>
-  //             </div>
-  //         </div>
-
-  return (
-   
-
-    <div className="App">
-      <header className="App-header">
-
-      {isModalOpen && ( 
-         <ModalComponent onConfirm={closeModal} name={name}  color={color}/>     
-        )}
-        
-        <form onSubmit={onSubmitHandler}>
-          Enter your Name: <input  id="username"
-            type="text"
-            value={name}
-            onChange={nameChangeHandler} />
-         
-          Enter your Favorite Color: 
-          <input id="color"
-            type="text"
-            value={color}
-            onChange={colorChangeHandler} />
-          <button>Enter</button>
-        </form>
-        
-
-      </header>
-    </div>
-  );
+				<form onSubmit={onSubmitHandler}>
+					Enter your Name:{" "}
+					<input
+						id="username"
+						type="text"
+						value={name}
+						onChange={nameChangeHandler}
+					/>
+					Enter your Favorite Color:
+					<input
+						id="color"
+						type="text"
+						value={color}
+						onChange={colorChangeHandler}
+					/>
+					<button>Enter</button>
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/modalComponent.js
+++ src/modalComponent.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './ModalComponent.css' ;
+import React from "react";
+import ReactDOM from "react-dom";
+import "./ModalComponent.css";
 
-const Backdrop = (props)=>{
-    return(
-      //className="backdrop" onClick={closeModal}
-      <div className="backdrop" onClick={props.onConfirm} />
-    )
-  }
-  const ModalOverLay = (props)=>{
-    return(
-      <div className="modal">
-      <h2>Hey, {props.name}! How is it going?</h2>
-      <p>Your favorite color is {props.color}.</p>
-      <button onClick={props.onConfirm}>Close</button>
-    </div>
-    )
-  }
+const Backdrop = (props) => {
+	return <div className="backdrop" onClick={props.onConfirm} />;
+};
+const ModalOverLay = (props) => {
+	return (
+		<div className="modal">
+			<h2>"Howdy, {props.name}! Welcome to the Modal."</h2>
+			<p>Your favorite color is {props.color}.</p>
+			<button onClick={props.onConfirm}>Close</button>
+		</div>
+	);
+};
 const ModalComponent = (props) => {
-  return (
-    <React.Fragment>
-
-        {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById("backdrop-root"))}
-        {ReactDOM.createPortal(<ModalOverLay name={props.name}  color={props.color} onConfirm={props.onConfirm}/>, document.getElementById("overlay-root"))}
-
-  </React.Fragment>
-  )
-}
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById("backdrop-root")
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverLay
+					name={props.name}
+					color={props.color}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById("overlay-root")
+			)}
+		</React.Fragment>
+	);
+};
 
 export default ModalComponent;
